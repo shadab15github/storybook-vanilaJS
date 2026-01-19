6e1c0bdd3525a0a4309db7df31ef897f5e1a4c84
shadab15github <shadab150102@gmail.com>
2026-01-19 14:45:47 +0530

    Create a New Component Called Story Button

---
+++ src/stories/storyButton/StoryButton.js
@@ -0,0 +1,21 @@
+import "./button.css";
+
+export const createStoryButton = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  onClick,
+}) => {
+  const btn = document.createElement("button");
+  btn.type = "button";
+  btn.innerText = label;
+  btn.addEventListener("click", onClick);
+
+  const mode = primary ? "story-button--primary" : "story-button--secondary";
+  btn.className = ["story-button", `story-button--${size}`, mode].join(" ");
+
+  btn.style.backgroundColor = backgroundColor;
+
+  return btn;
+};
